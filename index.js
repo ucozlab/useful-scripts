71ffd4df6f018c0735da27f2f5d7bb5e58517040
Artem <submerged311@gmail.com>
2017-08-02 14:04:11 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Получает степень прокрутки страницы.
+function getScrollPercent() {
+  return (
+    (document.documentElement.scrollTop || document.body.scrollTop) 
+    / ( (document.documentElement.scrollHeight || document.body.scrollHeight) 
+    - document.documentElement.clientHeight) 
+    * 100);
+}
